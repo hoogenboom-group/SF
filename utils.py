e1aa1c6ea6ebf5705c6da806f421a2854b3027f8
EvdWee <42221627+EvdWee@users.noreply.github.com>
2023-10-30 15:26:55 +0100

    added notebooks with rescaling of bead stacks + updated functions in util.py

--- utils.py
+++ utils.py
@@ -3,10 +3,17 @@ import logging
 from skimage import io
 from pathlib import Path
 from tqdm.notebook import tqdm
+from scipy.optimize import curve_fit
 import tifffile
 import matplotlib.pyplot as plt
+import psf_extra as psfe
 
-__all__ = ['rescale_stack',
+__all__ = ['plot_int_histograms',
+           'exp_func',
+           'max_rolling2',
+           'compensate_int_loss',
+           'plot_stacks_MIPs',
+           'rescale_stack',
            'plot_stacks_overlay',
            'eight_bit_as',
            'save_stack',
@@ -16,49 +23,169 @@ __all__ = ['rescale_stack',
            'diel_mean', 
            'diel_median', 
            'scaling_factor', 
+           'scaling_factor_from_nfp',
            'load_stack']
 
-def rescale_stack(stack, NA, n1, n2, lam_0, ps_z):
-    # make list of apparent focal positions (NFP) of stack
+fire = psfe.get_Daans_special_cmap()
+
+def plot_int_histograms(stack1,stack2,names):
+    stack1_list = stack1.flatten()
+    stack2_list = stack2.flatten()
+    fig,axs=plt.subplots(1,2)
+    axs[0].hist(stack1_list,bins=200)
+    axs[0].set_yscale('log')
+    axs[0].set_title(names[0])
+    axs[0].set_xlabel('Intensity (a.u.)')
+    axs[0].set_ylabel('Counts')
+    axs[0].set_xlim(0,1)
+    axs[1].hist(stack2_list,bins=200)
+    axs[1].set_yscale('log')
+    axs[1].set_title(names[1])
+    axs[1].set_xlabel('Intensity (a.u.)')
+    axs[1].set_ylabel('Counts')
+    axs[1].set_xlim(0,1)
+    plt.tight_layout()
+    plt.show()
+    return
+
+def exp_func(x,a,b,c):
+    return c*a**(-b*x)
+
+def max_rolling2(A,K):
+    rollingmax = np.array([max(A[j:j+K]) for j in range(len(A)-K)])
+    return rollingmax
+
+def compensate_int_loss(stack,kernel_size=25,fit=False,skip_slides=10, plot=False):
+    intensity=[]
+    for slice in stack:
+        intensity.append(np.mean(slice))
+    zs = np.arange(1,len(intensity)+1,1)
+    rol_max_intensity=max_rolling2(intensity,kernel_size)
+    difference = len(intensity)-len(rol_max_intensity)
+    list0=np.zeros(difference) + rol_max_intensity[0]
+    rol_max_intensity_padded = np.append(list0,rol_max_intensity)
+    #fit rolling maximum with exponential:
+    if fit==True:
+        
+        popt,pcov=curve_fit(exp_func,zs,rol_max_intensity_padded)#,bounds=([0,0,0],[1,1,np.inf]))
+        if plot==True: plt.plot(exp_func(zs,*popt),label='exp fit')
+
+        fit_intensities = exp_func(zs,*popt)
+    else:
+        fit_intensities = rol_max_intensity_padded
+    if plot==True:
+        plt.plot(zs,rol_max_intensity_padded,label='rolling maximum')
+        plt.plot(zs,intensity,label='intensity')
+        plt.legend()
+        #plt.yscale('log')
+        plt.show()
+    stack_int=np.zeros(np.shape(stack))
+    for i in range(len(stack)):
+        if i < len(stack)-skip_slides:
+            stack_int[i,:,:] = np.divide(stack[i,:,:],fit_intensities[i])
+        else: 
+            stack_int[i,:,:] = np.divide(stack[i,:,:],fit_intensities[len(stack)-skip_slides])
+    
+    return stack_int, intensity,fit_intensities
+
+def plot_stacks_MIPs(stack_one, stack_two, stack_names, planes, ps_xy_one, ps_z_one, ps_xy_two, ps_z_two):
+
+    shape_one = np.shape(stack_one)
+    shape_two = np.shape(stack_two)
+
+    fig,axs=plt.subplots(2,2)
+    fig.set_figheight(10)
+    fig.set_figwidth(10)
+
+    for i in range(len(stack_names)):
+        for j in range(len(planes)):
+            if planes[j] == 'xy': 
+                mip_axis = 0 
+                if i==1: extent = [0, shape_two[2]*ps_xy_two, 0, shape_two[1]*ps_xy_two]
+                else: extent = [0, shape_one[2]*ps_xy_one, 0, shape_one[1]*ps_xy_one]
+            else: 
+                mip_axis = 1
+                if i==1: extent = [0, shape_two[2]*ps_xy_two, 0, shape_two[0]*ps_z_two]
+                else: extent = [0, shape_one[2]*ps_xy_one, 0, shape_one[0]*ps_z_one]
+
+            axs[j,i].imshow(np.max([stack_one,stack_two][i], axis=mip_axis),cmap=fire,extent=extent)
+            axs[j,i].set_title(stack_names[i])
+            axs[j,i].set_xlabel(planes[j][:1]+r' ($\mu$m)')
+            axs[j,i].set_ylabel(planes[j][1:]+r' ($\mu$m)')
+    plt.tight_layout()
+    plt.show()
+    return
+
+def rescale_stack(stack, NA, n1, n2, lam_0, ps_z,crit = 'Lyakin'):
+    # make list of apparent focal positions (NFP) of  input stack (with ref index mismatch)
     shape_stack = np.shape(stack)
     nfp_stack = np.arange(shape_stack[0])*ps_z
-    nfp_stack[0]=0.00000001 # prevent function from blowing up
+    nfp_stack[0]=0.00000001 # prevent SF function from blowing up
     
-    # calculate depth-dependent scaling factor
-    sf_stack = scaling_factor(nfp_stack, NA,n1,n2,lam_0)
-    nfp_stack[0]=0.000000 # prevent function from blowing up
-    sf_stack[0]=sf_stack[1] # equation get unphysical when z -> 0, therefore replace with next number (cheating, I know)
+    # calculate depth-dependent scaling factor for each NFP
+    sf_stack = scaling_factor_from_nfp(nfp_stack, NA,n1,n2,lam_0,crit=crit)
+    nfp_stack[0]=0.0 # make first value of NFP array zero again (funky, I know)
     
-    # calculate actual focal position (AFP) of stack
-    afp_stack = nfp_stack*sf_stack
+    # calculate actual focal position (AFP) of stack using scaling factor and NFP
+    afp_stack = np.multiply(nfp_stack,sf_stack)
 
     # make new stack rescaled data will be added to
-    # use step size of original stack and range calculated from depth-dependent scaling for last slide
-    final_sf = sf_stack[-1]
+    # use step size of original stack and range calculated from depth-dependent scaling of last slide in input stack
     afp_new_stack = np.arange(0,afp_stack[-1],ps_z)
-
+    
     # make empty array we will fill with intensities of rescaled stack
     stack_rescaled=np.empty([len(afp_new_stack),shape_stack[1],shape_stack[2]])
-    
+
     # put intensities of rescaled stack into new evenly spaced stack:
     for i in range(len(afp_new_stack)):
         if i == 0: #first slide, no rescaling
             stack_rescaled[i] = stack[0]
         else:
-            afp_slide = afp_new_stack[i] #get depth of new stack
-            #find two nearest slides in air stack and their z position (AFP)
-            index, value =min(enumerate(afp_stack), key=lambda x: abs(x[1]-afp_slide))
+            afp_slide = afp_new_stack[i] #get AFP of slice in new stack
+            
+            #find two nearest slices in mismatched stack and their AFPs
+            index, value =min(enumerate(afp_stack), key=lambda x: abs(x[1]-afp_slide)) #get the index of the closest AFP value in the AFP list of the mismatched stack
+            #get the indices of the slices in the mismatch stack surrounding the AFP value in the new stack
             if afp_slide > value: indices = [index, index+1]
             else: indices = [index-1, index]
+            #get the corresponding AFP values in the mismatched stack
             value_under, value_upper = afp_stack[indices[0]],afp_stack[indices[1]]
+            #calculate the AFP distance between the slice in the new stack and the surrounding slices in the mismatched stack
             dz_under, dz_upper = afp_slide - value_under, value_upper - afp_slide
+            
+            # we will use inverse distance weighting with power of 1 to interpolate the intensity in the new stack: https://en.wikipedia.org/wiki/Inverse_distance_weighting :
             dz_under_inv, dz_upper_inv=1/dz_under, 1/dz_upper
             dz_sum = dz_under + dz_upper
             dz_inv_sum = dz_under_inv + dz_upper_inv
-            # make new slide from two closest slides
+            # make interpolate intensities in new slide from the two closest slices in the mismatched stack:
             new_slide = np.divide(np.multiply(stack[indices[0],:,:],dz_under_inv)+np.multiply(stack[indices[1],:,:],dz_upper_inv),dz_inv_sum)
-            #this used inverse distance weighting with power of 1: https://en.wikipedia.org/wiki/Inverse_distance_weighting
+            
+            #save to new stack
             stack_rescaled[i] = new_slide
+            
+            #debug
+            # print('AFP new stack (um) ', afp_slide,# 'Mean intensity slide: ', np.mean(new_slide)*10000,
+            #   '\nAFP under old stack', value_under,# 'Mean intensity slide under: ', np.mean(stack[indices[0],:,:])*10000,
+            #       '\nAFP upper old stack',value_upper,# 'Mean intensity slide upper: ', np.mean(stack[indices[0],:,:])*10000,
+            #       '\n' )
+            
+            #plot new slices and surrounding slices
+            # fig,axs=plt.subplots(1,3)
+            # fig.set_figheight(10)
+            # fig.set_figwidth(20)
+            # axs[0].imshow(new_slide)
+            # axs[0].set_title('New stack index: '+str(i))
+            # axs[1].set_title('Old stack index under: '+str(indices[0]))
+            # axs[2].set_title('Old stack index upper: '+str(indices[1]))
+            # axs[1].imshow(stack[indices[0],:,:])
+            # axs[2].imshow(stack[indices[1],:,:])
+            # plt.show()
+            
+            #print indices in both stacks
+            # print('\nNew stack index: '+str(i))
+            # print('Old stack index under: '+str(indices[0]))
+            # print('Old stack index upper: '+str(indices[1]))
+            
     return stack_rescaled, afp_new_stack, afp_stack, nfp_stack
 
 def plot_stacks_overlay(stack_rescaled,stack, afp_new_stack,afp_stack,nfp_stack,ps_xy,ps_z):
@@ -209,8 +336,11 @@ def f1f2_av(nn1,nn2,NA):
     return f1f2
 
 def diel_mean(z,n_im,n_sample,NA):
+    if NA > n_sample: 
+        print("Numerical aperture larger than sample refractive index, Diel mean cannot be computed.")
+        return
     sum=0
-    number_of_rays=10000 # paper uses 100, but this is still doable.
+    number_of_rays=10 # paper uses 100, but this is still doable.
     for i in range(number_of_rays):
         k=i+1
         top     =  np.tan(np.arcsin(np.divide((NA*k),(np.multiply(number_of_rays,n_im)))))
@@ -246,6 +376,34 @@ def scaling_factor(z, NA,n1,n2,lam_0):
         elif n2overn1 > 1:sf[i] = np.min([np.real(sf_univ[i]),np.real(sf_crit)])
         else: sf[i]=1
     return sf
+
+def scaling_factor_from_nfp(z, NA,n1,n2,lam_0,crit = 'Lyakin'):
+    #dzeta vs NFP
+    n2overn1 = np.divide(n2,n1)
+    m = np.emath.sqrt(np.subtract(np.power(n2,2),np.power(n1,2)))
+    
+    if n2overn1 < 1: delta = np.multiply(-1,np.divide(lam_0/4, np.multiply(n1,z)))
+    else: delta = np.divide(lam_0/4, np.multiply(n1,z))
+    one_plus_delta = np.add(1,delta)
+    
+    first_term = np.multiply(n2overn1,one_plus_delta)
+    sec_term = np.multiply(np.divide(m,n1),np.emath.sqrt(np.multiply(delta,np.add(2,delta))))
+    sf_univ = np.add(first_term, sec_term)
+    
+    sf = np.zeros(len(z))
+    if crit != 'None': #cap off exploding SF for small depths
+        if crit == 'Loginov': #use Loginov's critical value
+            sf_crit = np.divide(n1-np.emath.sqrt(np.power(n1,2)-np.power(NA,2)),
+                                    n2-np.emath.sqrt(np.power(n2,2)-np.power(NA,2)))
+        elif crit == 'Lyakin': # use Lyakin/Stallinga's value
+            sf_crit = Lyakin([0],n2,n1,NA)[0]
+        for i in range(len(sf)):
+            if n2overn1 < 1: sf[i] = np.max([np.real(sf_univ[i]),np.real(sf_crit)])
+            elif n2overn1 > 1:sf[i] = np.min([np.real(sf_univ[i]),np.real(sf_crit)])
+            else: sf[i]=1
+    else: sf = np.real(sf_univ) #when no capping is performed
+    return sf
+
 
 def load_stack(file_pattern):
     
